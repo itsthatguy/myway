ec614e64fb652812b4b19da49289e566bb9642c3
Kevin Altman <kevin@itsthatguy.com>
2013-06-06 16:57:05 -0400

    updating myway to current setup

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source 'http://rubygems.org'
 
-gem 'middleman', '~>2.0.15.1'
-gem 'middleman-blog'
-gem 'slim'
+gem 'middleman', '~>3.0.14'
--- config.rb
+++ config.rb
@@ -1,17 +1,72 @@
-activate :blog
-# set :blog_permalink, ":year/:month/:day/:title.html"
-# set :blog_summary_separator, /READMORE/
-# set :blog_summary_length, 500
+### 
+# Compass
+###
 
-page "/feed.xml", :layout => false
+# Susy grids in Compass
+# First: gem install compass-susy-plugin
+# require 'susy'
 
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
 
-activate :directory_indexes
+###
+# Haml
+###
 
-# custom middleman blog setup
-set :erb, :layout_engine => :slim
-set :blog_layout_engine, "slim"
-set :blog_index_template, "blog_index"
+# CodeRay syntax highlighting in Haml
+# First: gem install haml-coderay
+# require 'haml-coderay'
+
+# CoffeeScript filters in Haml
+# First: gem install coffee-filter
+# require 'coffee-filter'
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+###
+# Page command
+###
+
+# Per-page layout changes:
+# 
+# With no layout
+# page "/path/to/file.html", :layout => false
+# 
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+# 
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Change the CSS directory
+# set :css_dir, "alternative_css_directory"
+
+# Change the JS directory
+# set :js_dir, "alternative_js_directory"
+
+# Change the images directory
+# set :images_dir, "alternative_image_directory"
 
 # Build-specific configuration
 configure :build do
@@ -23,4 +78,15 @@ configure :build do
   
   # Enable cache buster
   # activate :cache_buster
+  
+  # Use relative URLs
+  # activate :relative_assets
+  
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
 end
